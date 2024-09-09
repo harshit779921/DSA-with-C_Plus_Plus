a0dfa405b6055c5f6c2ef089faf96a4816c7c21c
harshit779921 <pusphadevipandey8@gmail.com>
2024-09-09 23:54:38 +0530

    pattern 10

--- patterns by Love/q10.cpp
+++ patterns by Love/q10.cpp
@@ -6,18 +6,12 @@ int main()
 {
     int n;
     cin >> n;
-    int i = 1;
-
-    while (i <= n)
+    for (int i = 1; i <= n; i++)
     {
-        int j = i;
-
-        while (j < 2 * i)
+        for (int j = 1; j <= 2*i; j++)
         {
-            cout << j << " ";
-            j = j + 1;
+            cout << i+j-1<<" ";
         }
         cout << endl;
-        i = i + 1;
     }
 }
