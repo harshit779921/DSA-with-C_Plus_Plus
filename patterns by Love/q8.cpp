1a4755d547fbfb034e04dc08477ce5fcc7687ef4
harshit779921 <pusphadevipandey8@gmail.com>
2024-09-09 23:45:39 +0530

    pattern 8

--- patterns by Love/q8.cpp
+++ patterns by Love/q8.cpp
@@ -4,18 +4,16 @@ int main()
 {
     int n;
     cin >> n;
-    int i = 1;
     int count = 1;
-    while (i <= n)
+
+    for (int i = 1; i <= n; i++)
     {
-        int j = 1;
-        while (j <= i)
+        for (int j = 1; j <= i; j++)
         {
-            cout << count << " ";
-            j = j + 1;
-            count = count + 1;
+            cout << count ;
+            count = count+1;
         }
         cout << endl;
-        i = i + 1;
     }
 }
+    
