0fdc4fc36905a169e9359efb66f3d43b7c15cc43
harshit779921 <pusphadevipandey8@gmail.com>
2024-09-10 00:04:22 +0530

    pattern 13

--- patterns by Love/q13.cpp
+++ patterns by Love/q13.cpp
@@ -4,19 +4,14 @@ int main()
 {
     int n;
     cin >> n;
-    int i = 1;
-
-    while (i <= n)
+    
+    for (int i = 1; i <= n; i++)
     {
-        int j = 1;
-
-        while (j <= n)
+        for (int j = 1; j <= n; j++)
         {
             char ch = 'A' + j - 1;
             cout << ch << " ";
-            j = j + 1;
         }
         cout << endl;
-        i = i + 1;
     }
 }
