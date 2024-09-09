9e059081e31874b3069dd14bf03be14fbcd0e5b2
harshit779921 <pusphadevipandey8@gmail.com>
2024-09-10 01:07:01 +0530

    pattern 24

--- patterns by Love/q24.cpp
+++ patterns by Love/q24.cpp
@@ -5,28 +5,24 @@ int main()
     int n;
     cin >> n;
 
-    int i = 1;
-    while (i <= n)
+    for (int i = 1; i <= n; i++)
     {
-        int space = n - i;
-        while (space)
+        // Print spaces
+        for (int space = n - i; space > 0; space--)
         {
             cout << " ";
-            space = space - 1;
         }
-        int j = 1;
-        while (j <= i)
+
+        for (int j = 1; j <= i; j++)
         {
             cout << j;
-            j = j + 1;
         }
-        int k = i - 1;
-        while (k)
+
+        for (int k = i - 1; k > 0; k--)
         {
             cout << k;
-            k = k - 1;
         }
+
         cout << endl;
-        i = i + 1;
     }
 }
