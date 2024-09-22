5874e3f551d777ff6e39d1121ea2e336871bab0a
harshit779921 <harshitpandey072003@gmail.com>
2024-09-22 22:42:52 +0530

    Revert "pattern 13"
    This reverts commit 0fdc4fc36905a169e9359efb66f3d43b7c15cc43.

--- patterns by Love/q13.cpp
+++ patterns by Love/q13.cpp
@@ -4,14 +4,19 @@ int main()
 {
     int n;
     cin >> n;
-    
-    for (int i = 1; i <= n; i++)
+    int i = 1;
+
+    while (i <= n)
     {
-        for (int j = 1; j <= n; j++)
+        int j = 1;
+
+        while (j <= n)
         {
             char ch = 'A' + j - 1;
             cout << ch << " ";
+            j = j + 1;
         }
         cout << endl;
+        i = i + 1;
     }
 }
